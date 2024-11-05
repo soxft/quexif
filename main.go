b2ef96024bcb45a85fad93803807435129adbaea
soxft <email@xcsoft.top>
2024-11-05 10:49:22 +0800

    :children_crossing: Perf: 添加安全提示

--- main.go
+++ main.go
@@ -169,6 +169,15 @@ func main() {
 		log.Fatal("Usage: exif_pass <directory>")
 	}
 
+	// 安全 QA
+	log.Println("请确保已经设置了快照，程序将会直接修改文件的 exif 元数据, 建议在使用前选择少量照片进行测试后再使用")
+	log.Println("您确认要继续吗? (y/n)")
+	var confirm string
+	fmt.Scanln(&confirm)
+	if confirm != "y" {
+		log.Fatal("已取消")
+	}
+
 	// 读取目录
 	dir := os.Args[1]
 	files, err := os.ReadDir(dir)
